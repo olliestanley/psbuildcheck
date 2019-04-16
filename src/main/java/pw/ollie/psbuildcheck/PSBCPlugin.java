6e6e087d75ac06be5e49b567735a6014a571fde8
Oliver <o.dzineit@gmail.com>
2019-04-16 13:15:49 +0100

    Basis of the project

---
+++ src/main/java/pw/ollie/psbuildcheck/PSBCPlugin.java
@@ -0,0 +1,9 @@
+package pw.ollie.psbuildcheck;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PSBCPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
